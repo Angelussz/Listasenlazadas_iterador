b8ff26dcf684d2ab0de87d65add4bb9d552d096e
Angelussz <38799735+Angelussz@users.noreply.github.com>
2019-06-17 13:33:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //codigo guiado de https://ronnyml.wordpress.com/2009/07/04/listas-enlazadas-clase-lista-en-c/
-
+//https://es.wikipedia.org/wiki/Iterador_(patrón_de_diseño)
+//https://es.wikibooks.org/wiki/Programación_en_C%2B%2B/Sobrecarga_de_Operadores
 #include "iterador.h"
 using namespace std;
 int main()
